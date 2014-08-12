aab0d3a5d9c39cb90b2307149bd120124b5116f1
Michael Williams <dinosaur@riseup.net>
2014-08-12 23:51:49 +0000

    s/Service/Feather

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 var debug = require('debug')('oa-service');
 
-function Service (store) {
+function Feather (store) {
   debug("constructor", store);
   
   // call new constructor if not already
-  if (!(this instanceof Service)) {
-    return new Service(store);
+  if (!(this instanceof Feather)) {
+    return new Feather(store);
   }
 
   this.store = store;
   this.name = store.name;
 };
 
-Service.prototype.setup = function (app) {
+Feather.prototype.setup = function (app) {
   debug("setup", app);
   this.app = app;
 };
 
-Service.prototype.find = function (params) {
+Feather.prototype.find = function (params) {
   params = params || {};
   return this.store.find(params)
   ;
 };
 
-Service.prototype.get = function (id, params) {
+Feather.prototype.get = function (id, params) {
   debug("get", id, params);
   params = params || {};
 
@@ -38,7 +38,7 @@ Service.prototype.get = function (id, params) {
   ;
 };
 
-Service.prototype.create = function (body, params) {
+Feather.prototype.create = function (body, params) {
   debug("create", body, params);
   params = params || {};
 
@@ -46,7 +46,7 @@ Service.prototype.create = function (body, params) {
   ;
 },
 
-Service.prototype.update = function (id, body, params) {
+Feather.prototype.update = function (id, body, params) {
   debug("update", id, body, params);
   params = params || {};
 
@@ -63,7 +63,7 @@ Service.prototype.update = function (id, body, params) {
   ;
 };
 
-Service.prototype.remove = function (id, params) {
+Feather.prototype.remove = function (id, params) {
   debug("remove", id, params);
   params = params || {};
 
@@ -71,4 +71,4 @@ Service.prototype.remove = function (id, params) {
   ;
 };
 
-module.exports = Service;
+module.exports = Feather;
